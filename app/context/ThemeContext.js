a0df2cee5ac5172378bc0d81550b9578cee5b8e7
Amir <vibelessyoung@gmail.com>
2026-07-20 22:30:12 +0330

    start handling light-dark

--- app/context/ThemeContext.js
+++ app/context/ThemeContext.js
@@ -1,11 +1,16 @@
 "use client";
 
 import { createContext, useState } from "react";
+import { useEffect } from "react";
 
 export const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
   const [theme, setTheme] = useState("dark");
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", theme === "dark");
+    document.documentElement.classList.add(theme);
+  }, [theme]);
 
   return (
     <ThemeContext.Provider
