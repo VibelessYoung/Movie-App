b8bb04850ace882987407120be511ee44284629d
Amir <vibelessyoung@gmail.com>
2026-07-21 19:07:12 +0330

    handle dark-light for trending page

--- app/trending/page.js
+++ app/trending/page.js
@@ -14,9 +14,9 @@ export default async function TrendingPage({ searchParams }) {
   return (
     <main className="mx-auto max-w-7xl px-6 py-12">
       <div className="mb-12">
-        <h1 className="text-5xl font-black text-white">🔥 Trending Movies</h1>
+        <h1 className="text-5xl font-black text-white light:text-black">🔥 Trending Movies</h1>
 
-        <p className="mt-4 text-zinc-400">
+        <p className="mt-4 text-zinc-400 light:text-black/50">
           Discover the most popular movies right now.
         </p>
       </div>
