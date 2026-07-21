47560286411b0e4faa19df5088be9e99bc573f15
Amir <vibelessyoung@gmail.com>
2026-07-21 19:02:44 +0330

    handle dark-light for search page

--- app/components/SearchResults.js
+++ app/components/SearchResults.js
@@ -18,7 +18,7 @@ export default function SearchResults({ movies }) {
   return (
     <>
       <div className="mx-auto mb-14 max-w-3xl">
-        <div className="flex items-center rounded-2xl border border-zinc-700 bg-zinc-900 px-5 focus-within:border-red-600">
+        <div className="flex items-center rounded-2xl border border-zinc-700 bg-zinc-900 px-5 light:bg-gray-100 light:focus-within:border-black focus-within:border-red-600">
           <Search size={22} className="text-zinc-500" />
 
           <input
@@ -26,15 +26,15 @@ export default function SearchResults({ movies }) {
             placeholder="Search by movie title..."
             value={query}
             onChange={(e) => setQuery(e.target.value)}
-            className="w-full bg-transparent px-4 py-5 text-white outline-none placeholder:text-zinc-500"
+            className="w-full bg-transparent px-4 py-5 text-white light:text-black light:bg-gray-100 outline-none placeholder:text-zinc-500"
           />
         </div>
       </div>
 
       <div className="mb-8 flex items-center justify-between">
-        <h2 className="text-3xl font-bold text-white">Results</h2>
+        <h2 className="text-3xl font-bold text-white light:text-black">Results</h2>
 
-        <span className="rounded-xl bg-zinc-900 px-4 py-2 text-zinc-400">
+        <span className="rounded-xl bg-zinc-900 px-4 py-2 light:bg-gray-200 light:text-black text-zinc-400">
           {filteredMovies.length} Movies
         </span>
       </div>
--- app/search/page.js
+++ app/search/page.js
@@ -7,9 +7,9 @@ export default async function SearchPage() {
   return (
     <main className="mx-auto max-w-7xl px-6 py-12">
       <div className="mb-12 text-center">
-        <h1 className="text-5xl font-black text-white">Search Movies</h1>
+        <h1 className="text-5xl font-black text-white light:text-black">Search Movies</h1>
 
-        <p className="mt-4 text-zinc-400">
+        <p className="mt-4 text-zinc-400 light:text-black/50">
           Search through our movie collection.
         </p>
       </div>
